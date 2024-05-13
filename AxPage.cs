cc53aa14f26536afa2afebeeb4ce53840cf1daee
Tomáš Kouba <tomas.kouba@vinci-energies.net>
2024-05-13 15:06:48 +0200

    Update BindableProps and test new feature - bindable read-only

--- AxPage.cs
+++ AxPage.cs
@@ -1,6 +1,7 @@
 ﻿using BindableProps;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,21 @@ namespace MauiAppReadOnlyProperty
 {
     public partial class AxPage : ContentPage
     {
-        public static readonly BindablePropertyKey SizePropertyKey = BindableProperty.CreateReadOnly(nameof(Size), typeof(SizeF), typeof(AxPage), SizeF.Zero);
-        public static readonly BindableProperty SizeProperty = SizePropertyKey.BindableProperty;
+        // Manual read-only bindable property definition
+        //public static readonly BindablePropertyKey SizePropertyKey = BindableProperty.CreateReadOnly(nameof(Size), typeof(SizeF), typeof(AxPage), SizeF.Zero, BindingMode.OneWayToSource);
+        //public static readonly BindableProperty SizeProperty = SizePropertyKey.BindableProperty;
+        //public SizeF Size
+        //{
+        //    get => (SizeF)GetValue(SizeProperty);
+        //    private set => SetValue(SizePropertyKey, value);
+        //}
 
-        public SizeF Size
-        {
-            get => (SizeF)GetValue(SizeProperty);
-            private set => SetValue(SizePropertyKey, value);
-        }
+        // Generated read-only property definition with proper binding mode
+        [BindableReadOnlyProp(DefaultBindingMode = (int)BindingMode.OneWayToSource)]
+        SizeF _size;
 
-        [BindableProp]
-        bool _isLandscape = false; // <-- this property should also be read-only
+        [BindableReadOnlyProp]
+        bool _isLandscape = false;
 
         protected override void OnSizeAllocated(double width, double height)
         {
